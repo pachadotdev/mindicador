2801598024c046d5df02d9b7429a17883faf99c3
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-06-02 12:34:26 -0400

    remove non-ascii strings in available data

--- dev/00-generar-tablas.R
+++ dev/00-generar-tablas.R
@@ -6,9 +6,10 @@ library(data.table)
 
 use_directory("data-raw", ignore = T)
 
-download.file("https://mindicador.cl/api", "data-raw/indicadores.json")
+archivo_indicadores <- "data-raw/indicadores.json"
+if (!file.exists(archivo_indicadores)) download.file("https://mindicador.cl/api", archivo_indicadores)
 
-indicadores <- fromJSON("https://mindicador.cl/api")
+indicadores <- fromJSON(archivo_indicadores)
 indicadores <- indicadores[!names(indicadores) %in% c("version", "autor", "fecha")]
 
 codigos <- names(indicadores)
@@ -20,7 +21,7 @@ mindicador_indicadores <- map_df(
     d <- d[[1]]
 
     tibble(
-      nombre = d$nombre,
+      nombre = iconv(d$nombre, to = "ASCII//TRANSLIT"),
       codigo = d$codigo,
       unidad = d$unidad_medida
     )
@@ -31,4 +32,4 @@ mindicador_indicadores <- as.data.table(mindicador_indicadores)
 # sacado de tabla html en home de mindicador.cl
 mindicador_indicadores$desde <- c(1977,1990,1984,1988,1999,1928,1990,1997,2001,2012,2009,2009)
 
-use_data(mindicador_indicadores)
+use_data(mindicador_indicadores, overwrite = T)
